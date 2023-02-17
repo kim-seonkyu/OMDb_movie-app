eb85dca2f5eaf1f87076928acaa12a88d8a31a6f
kim-seonkyu <seonkyu93@gmail.com>
2023-02-18 00:11:16 +0900

    OMDb Movie App - About Page fin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Component } from "../core/core";
+import aboutStore from "../store/about";
 
 export default class Footer extends Component {
   constructor() {
@@ -7,12 +8,13 @@ export default class Footer extends Component {
     });
   }
   render() {
+    const { github, repository } = aboutStore.state;
     this.el.innerHTML = /* html */ `
             <div>
-                <a href="https://github.com/kim-seonkyu/OMDb_movie-app">GitHub Repository</a>
+                <a href="${repository}">GitHub Repository</a>
             </div>
             <div>
-                <a href="https://github.com/kim-seonkyu">
+                <a href="${github}">
                     ${new Date().getFullYear()}
                     KH
                 </a>
---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from "../core/core";
+
+export default new Store({
+  photo: "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png",
+  name: "KH / KimKyunHa",
+  email: "seonkyu93@gmail.com",
+  github: "https://github.com/kim-seonkyu",
+  repository: "https://github.com/kim-seonkyu/OMDb_movie-app",
+});
